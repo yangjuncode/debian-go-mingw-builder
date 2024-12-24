153b2e17bf78eab16d9631dbcab09ee758752a16
yangjun <290968698@qq.com>
2024-12-24 10:47:35 +0800

    make go & go-mingw in same dir

--- 1.21/Dockerfile.go
+++ 1.21/Dockerfile.go
@@ -15,5 +15,7 @@ COPY  patch/*.patch /usr/local/go/
 RUN cd /usr/local/go \
     && patch --verbose -p1 < /usr/local/go/00-apply_https___go_dev_cl_600296.patch \
     && patch --verbose -p1 < /usr/local/go/01-Revert_crypto_rand,runtime__switch_RtlGenRandom_for_ProcessPrng.patch \
+    && patch --verbose -p1 < /usr/local/go/02-fix__apply_https___go_dev_cl_600296.patch \
+    && patch --verbose -p1 < /usr/local/go/03-runtime__reserve_4kB_for_system_stack_on_windows-386.patch \
     && rm -rf /usr/local/go/*.patch
 
--- 1.21/build.sh
+++ 1.21/build.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
-go_version=1.21.13
+go_version=1.21.13-1
 
-docker build --progress=plain -t ghcr.io/yangjuncode/go:${go_version}  .
+docker build --progress=plain -t ghcr.io/yangjuncode/go:${go_version} -f Dockerfile.go .
+docker tag ghcr.io/yangjuncode/go:${go_version} ghcr.io/yangjuncode/go:1.21.13
+
+docker build --progress=plain --build-arg go_version=${go_version} -t ghcr.io/yangjuncode/go-mingw:${go_version} -f Dockerfile.go-mingw .
+docker tag ghcr.io/yangjuncode/go-mingw:${go_version} ghcr.io/yangjuncode/go-mingw:1.21.13
+
+#if has param -p, push to docker registry
+if [[ $1 == "-p" ]]; then
+    docker push ghcr.io/yangjuncode/go:${go_version}
+    docker push ghcr.io/yangjuncode/go:1.21.13
+    docker push ghcr.io/yangjuncode/go-mingw:${go_version}
+    docker push ghcr.io/yangjuncode/go-mingw:1.21.13
+fi
