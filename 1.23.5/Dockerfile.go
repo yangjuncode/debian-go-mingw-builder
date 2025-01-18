3eafa639d9c09fe8b436dc86e4d7577d6bf1c829
yangjun <290968698@qq.com>
2025-01-18 18:09:46 +0800

    1.23.5

---
+++ 1.23.5/Dockerfile.go
@@ -0,0 +1,19 @@
+ARG go_version=1.23.5
+FROM golang:${go_version}-bullseye
+
+LABEL org.opencontainers.image.source=https://github.com/yangjuncode/debian-go-mingw-builder/1.22
+
+USER root
+
+RUN set -o xtrace \
+    && apt-get clean \
+    && apt-get update \
+    && apt-get -y --allow-unauthenticated install patch
+
+COPY  patch/*.patch /usr/local/go/
+
+RUN cd /usr/local/go \
+    && for patch_file in *.patch; do \
+    patch --verbose -p1 < "/usr/local/go/$patch_file"; \
+    done \
+    && rm -rf /usr/local/go/*.patch
